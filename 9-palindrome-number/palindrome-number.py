055c96bb6349b5f7c30af28a47ef91c5fdce6e5e
adityakhot09 <adityakhot122@gmail.com>
2026-08-30 10:56:26 +0530

    Time: 11 ms (56.26%) | Memory: 12.1 MB (99.89%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -3,13 +3,4 @@ class Solution:
         if x < 0:
             return False
 
-        original = x
-        reverse = 0
-
-        while x > 0:
-            digit = x % 10
-            reverse = reverse * 10 + digit
-            x = x // 10
-
-        return original == reverse
-        
+        return str(x) == str(x)[::-1]
