7907f6dbc3040250fc26ad1a5eb293f37269534b
adityakhot09 <adityakhot122@gmail.com>
2026-09-11 23:27:33 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (11.22%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s):
+        stack = []
+
+        brackets = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for char in s:
+            if char in brackets:
+                if not stack or stack[-1] != brackets[char]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(char)
+
+        return len(stack) == 0
