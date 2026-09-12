6a8e5569532b1e5792346fe53f963ac3627e94f3
adityakhot09 <adityakhot122@gmail.com>
2026-09-12 22:03:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (91.87%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,11 +1,9 @@
 class Solution:
     def merge(self, nums1, m, nums2, n):
-        # Start from the end of both arrays
         i = m - 1
         j = n - 1
         k = m + n - 1
 
-        # Compare elements from the end
         while i >= 0 and j >= 0:
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
@@ -16,7 +14,6 @@ class Solution:
 
             k -= 1
 
-        # Copy remaining elements of nums2
         while j >= 0:
             nums1[k] = nums2[j]
             j -= 1
